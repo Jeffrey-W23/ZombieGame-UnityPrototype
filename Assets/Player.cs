a050427eeb7371237ba510b70d9ffff1e81bbeb7
Thomas Wiltshire <thomas.wiltshire14@icloud.com>
2017-09-04 17:27:35 +1000

    First Commit.

---
+++ Assets/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	// Player speed.
+	public float m_fspeed = 0.002f;
+
+	// Get rigid
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.W))
+		{
+			 rb.velocity += Vector2.up * m_fspeed * Time.deltaTime;
+		}
+
+		if (Input.GetKey(KeyCode.S))
+		{
+			rb.velocity -= Vector2.up * m_fspeed * Time.deltaTime;
+		}
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			rb.velocity -= Vector2.right * m_fspeed * Time.deltaTime;
+		}
+
+		if (Input.GetKey(KeyCode.D))
+		{
+			rb.velocity += Vector2.right * m_fspeed * Time.deltaTime;
+		}
+	}
+}
