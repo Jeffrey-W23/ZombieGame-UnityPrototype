c321d457718599db3e5065c13980f14200188098
Thomas Wiltshire <thomaswiltshire@Kochanski.local>
2017-09-07 02:55:36 +1000

    Added player rotate with the mouse.

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public GameObject target; //This will be your citizen
+	public Vector3 distance;
+
+	// Use this for initialization
+	void Start() 
+    {
+		distance = transform.position - target.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LastUpdate()
+    {
+		float newXPosition = target.transform.position.x - distance.x;
+		float newZPosition = target.transform.position.z - distance.z;
+
+		transform.position = new Vector3(newXPosition, transform.position.y, newXPosition);
+    }	
+}
